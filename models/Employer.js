41e3387166458429f8564cc8f91dd0b6e7beeb73
mjhwest89 <mjhwest89@gmail.com>
2021-12-08 13:13:12 +1030

    added employer model

---
+++ models/Employer.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+//why is DataTypes above, not being declared?
+const sequelize = require('../config/connection');
+
+class Employer extends Model {}
+
+Employer.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+    companyName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phoneNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+}, );
+
+module.exports = Employer;
