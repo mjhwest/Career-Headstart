67848a97ad4bc0676bdb27267cefda2bf53342ca
mjhwest89 <mjhwest89@gmail.com>
2021-12-16 23:35:21 +1030

    modfiled userRoutes, not sure what, maybe an enter somewhere

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -65,27 +65,6 @@ router.post('/signup', async(req, res) => {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 router.post('/login', async(req, res) => {
     try {
         const userData = await User.findOne({ where: { email: req.body.email } });
