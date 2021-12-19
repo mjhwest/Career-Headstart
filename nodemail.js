d9f3fe7bed5d1c5224ba612e85cb8e71e5975583
mjhwest89 <mjhwest89@gmail.com>
2021-12-19 19:42:52 +1030

    added user email detials for employers congrats email

--- nodemail.js
+++ nodemail.js
@@ -2,11 +2,11 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 const nodemailer = require("nodemailer");
 const {
-  Application,
-  JobSeeker,
-  User,
-  JobListing,
-  Employer,
+    Application,
+    JobSeeker,
+    User,
+    JobListing,
+    Employer,
 } = require("./models");
 
 require("dotenv").config();
@@ -15,74 +15,69 @@ require("dotenv").config();
 //step 1: create transporter, what connects to user service
 //this is oly for gmail.
 let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    },
 });
 
 
 
 const constructEmail = (applicationJSON) => {
-  const employer = applicationJSON.joblisting.employer;
+    const employer = applicationJSON.joblisting.employer;
 
-  const jobseeker = applicationJSON.jobseeker;
+    const jobseeker = applicationJSON.jobseeker;
 
-  return {
-    from: "careerheadstartboss@gmail.com",
-    to: employer.user.email,
-    subject: "One New Job Application",
-    text: `Congratulations, ${employer.user.firstName}, 
+    return {
+        from: "careerheadstartboss@gmail.com",
+        to: employer.user.email,
+        subject: "One New Job Application",
+        text: `Congratulations, ${employer.user.firstName}, 
     
-        ${jobseeker.user.firstName} ${jobseeker.user.lastName} has applied for your advertised position ${applicationJSON.joblisting.jobTitle}.`,
-  };
+        ${jobseeker.user.firstName} ${jobseeker.user.lastName} has applied for your advertised position ${applicationJSON.joblisting.jobTitle}.
+        
+        Contact them via email : ${jobseeker.user.email}`,
+    };
 };
 
 
-const sendApplicationAlert = async (application_id) => {
-  //load application
+const sendApplicationAlert = async(application_id) => {
+    //load application
 
-  const loadedApplication = await Application.findByPk(application_id, {
-    include: [
-      {
-        model: JobSeeker,
-        include: [
-          {
-            model: User,
-          },
+    const loadedApplication = await Application.findByPk(application_id, {
+        include: [{
+                model: JobSeeker,
+                include: [{
+                    model: User,
+                }, ],
+            },
+            {
+                model: JobListing,
+                include: [{
+                    model: Employer,
+                    include: [{
+                        model: User,
+                    }, ],
+                }, ],
+            },
         ],
-      },
-      {
-        model: JobListing,
-        include: [
-          {
-            model: Employer,
-            include: [
-              {
-                model: User,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  });
-
-  if (!loadedApplication) {
-      console.log("No application found in the DB")
-  } else {
-    const applicationJSON = loadedApplication.toJSON();
-    // console.log(JSON.stringify(loadedApplication.toJSON(), null, 4))
-    const mailOptions = constructEmail(applicationJSON);
-    transporter.sendMail(mailOptions, function (err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("Email send");
-      }
     });
-  }
+
+    if (!loadedApplication) {
+        console.log("No application found in the DB")
+    } else {
+        const applicationJSON = loadedApplication.toJSON();
+        console.log(JSON.stringify(loadedApplication.toJSON(), null, 4))
+        const mailOptions = constructEmail(applicationJSON);
+        transporter.sendMail(mailOptions, function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Email send");
+            }
+        });
+    }
 };
 
 module.exports = sendApplicationAlert;
