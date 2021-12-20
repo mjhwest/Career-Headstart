e3248b16b5dde8b9dc4332aafbec7795a2b8cf4a
Carl Santiago <4518gg@gmail.com>
2021-12-20 15:42:13 +1030

    Fixed employer dashboard

--- models/index.js
+++ models/index.js
@@ -1,69 +1,78 @@
-const User = require('./User');
-const Employer = require('./Employer');
-const JobSeeker = require('./JobSeeker');
-const JobListing = require('./JobListing');
-const Application = require('./Application');
+const User = require("./User");
+const Employer = require("./Employer");
+const JobSeeker = require("./JobSeeker");
+const JobListing = require("./JobListing");
+const Application = require("./Application");
 
 // ---------------------------------------------
 User.hasOne(Employer, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Employer.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 // ---------------------------------------------
 User.hasOne(JobSeeker, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 JobSeeker.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: "user_id",
 });
 
 // ---------------------------------------------
 
 Employer.hasMany(JobListing, {
-    foreignKey: 'listed_by',
-    onDelete: 'CASCADE'
+  foreignKey: "listed_by",
+  onDelete: "CASCADE",
 });
 
 JobListing.belongsTo(Employer, {
-    foreignKey: 'listed_by'
+  foreignKey: "listed_by",
 });
 
 // ---------------------------------------------
 
 JobSeeker.belongsToMany(JobListing, {
-    through: {
-        model: Application,
-        unique: true,
-    },
-    as: 'applications',
-    foreignKey: 'applicant_id'
+  through: {
+    model: Application,
+    unique: true,
+  },
+  as: "applications",
+  foreignKey: "applicant_id",
 });
 
 JobListing.belongsToMany(JobSeeker, {
-    through: {
-        model: Application,
-        unique: true,
-    },
-    as: 'applicants',
-    foreignKey: 'listing_id'
+  through: {
+    model: Application,
+    unique: true,
+  },
+  as: "applicants",
+  foreignKey: "listing_id",
+});
+
+JobSeeker.hasMany(Application, {
+  foreignKey: "applicant_id",
+  onDelete: "CASCADE",
 });
 
 Application.belongsTo(JobSeeker, {
-    foreignKey: "applicant_id"
-})
+  foreignKey: "applicant_id",
+});
+
+JobListing.hasMany(Application, {
+  foreignKey: "listing_id",
+  onDelete: "CASCADE",
+});
 
 Application.belongsTo(JobListing, {
-    foreignKey: "listing_id"
-})
+  foreignKey: "listing_id",
+});
 
 // ---------------------------------------------
 
-module.exports = { User, Employer, JobSeeker, JobListing, Application }
+module.exports = { User, Employer, JobSeeker, JobListing, Application };
